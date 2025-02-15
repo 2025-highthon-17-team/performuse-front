dbb964f2c1804bbebafba57539e23ca85daaa204
kimgh06 <kkh061101@naver.com>
2025-02-15 20:07:23 +0900

    add: pretendard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import Sen from "../styles/sementic.module.css";
 
 export default function Root() {
   return (
-    <div className={`${Sen.text_primary_normal} ${Sen.title_1}`}>
-      <h1>Home</h1>
+    <div className={`${Sen.text_primary_normal} ${Sen.title_1} ${Sen.normal}`}>
+      나랏말싸미 듕귁에 달아 문자와로 서르 사맛디 아니할쌔 wertyuiop
     </div>
   );
 }
