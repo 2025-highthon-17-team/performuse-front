d9504a0fe519e7d5ea84675cb913fcbfc30d5be1
kimgh06 <kkh061101@naver.com>
2025-02-15 23:09:04 +0900

    make navigationbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Root from "./pages";
+import JoinPage from "./pages/join";
+import PointPage from "./pages/point";
+import ProfilePage from "./pages/profile";
+import Navigationbar from "./components/navigator/Navigationbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Root />} />
+          <Route path="/home" element={<Root />} />
+          <Route path="/qr" element={<JoinPage />} />
+          <Route path="/point" element={<PointPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<div>Not Found</div>} />
         </Routes>
       </BrowserRouter>
+      <Navigationbar />
     </div>
   );
 }
