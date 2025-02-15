31772b9aef8d15c39f77278066d0094be8097fcd
kimgh06 <kkh061101@naver.com>
2025-02-15 21:51:30 +0900

    made sdk login and signup

---
+++ src/hooks/client.ts
@@ -0,0 +1,92 @@
+import { useState, useCallback, useEffect } from "react";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+const client = axios.create({
+  baseURL: "http://43.201.215.142:8080",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export function useAxiosCRUD() {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const setAuthriazation = useCallback((token: string) => {
+    localStorage.setItem("token", token);
+    client.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+    setAuthriazation(token);
+    // eslint-disable-next-line
+    console.log("로그인 되어 있음.");
+  }, [setAuthriazation]);
+
+  const request = useCallback(
+    async (
+      method: "GET" | "POST" | "PUT" | "DELETE",
+      url: string,
+      requestData?: object,
+    ) => {
+      if (loading) return;
+      setLoading(true);
+
+      try {
+        const config: AxiosRequestConfig = {
+          method,
+          url,
+          data: ["POST", "PUT"].includes(method)
+            ? JSON.stringify(requestData)
+            : undefined,
+        };
+
+        const responseContainer: AxiosResponse = await client(config);
+        const res = responseContainer.data;
+        if (res.status !== 200) throw new Error("요청에 실패했습니다.");
+        return res.data;
+      } catch (err) {
+        // eslint-disable-next-line
+        console.error(err);
+        throw err;
+      } finally {
+        setLoading(false);
+      }
+    },
+    [loading],
+  );
+
+  const login = useCallback(
+    async (identifier: string, password: string) => {
+      const data = await request("POST", "/auth/login", {
+        identifier,
+        password,
+      });
+      if (!data) return;
+      setAuthriazation(data?.accessToken);
+    },
+    [request, setAuthriazation],
+  );
+
+  const signup = useCallback(
+    async (
+      identifier: string,
+      password: string,
+      email: string,
+      nickname: string,
+    ) => {
+      const data = await request("POST", "/auth/signup", {
+        identifier,
+        password,
+        email,
+        nickname,
+      });
+      if (!data) return;
+      setAuthriazation(data?.accessToken);
+    },
+    [request, setAuthriazation],
+  );
+
+  return { setAuthriazation, login, signup };
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import { useAxiosCRUD } from "../hooks/client";
 import Sen from "../styles/sementic.module.css";
 
 export default function Root() {
+  const { login } = useAxiosCRUD();
   return (
     <div className={`${Sen.text_primary_normal} ${Sen.title_1} ${Sen.normal}`}>
       나랏말싸미 듕귁에 달아 문자와로 서르 사맛디 아니할쌔 wertyuiop
+      <button onClick={() => login("qwerty", "q1w2e3r4t5y6!")}>
+        로그인 테스트
+      </button>
     </div>
   );
 }
