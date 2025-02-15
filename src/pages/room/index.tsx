fb202dd6d67dc2aea81a6b13ba081e951a49c717
kimgh06 <kkh061101@naver.com>
2025-02-16 01:57:48 +0900

    make quiz component

--- src/pages/room/index.tsx
+++ src/pages/room/index.tsx
@@ -1,10 +1,23 @@
 import { useParams } from "react-router-dom";
+import QuizComponent from "../../components/quiz/QuizComponent";
 
 export default function RoomPage() {
   const roomId = useParams<{ id: string }>().id;
+  const sendAnswer = (answer: string) => {
+    // eslint-disable-next-line
+    console.log(answer, roomId);
+  };
   return (
     <div>
-      <h1>Room {roomId}</h1>
+      <QuizComponent
+        title="퀴즈 제목"
+        question="퀴즈 질문"
+        point={100}
+        time={10}
+        type="주관식"
+        sendAnswer={sendAnswer}
+        selections={["선택1", "선택2", "선택3", "선택4"]}
+      />
     </div>
   );
 }
