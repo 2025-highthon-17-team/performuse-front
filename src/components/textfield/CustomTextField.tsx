8cf2e69b4832c2f2c64ab571d70757b2e62c93f8
dlrjsgml44 <leegeh1213@gmail.com>
2025-02-15 21:20:01 +0900

    feat: 텍스트 필드

---
+++ src/components/textfield/CustomTextField.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Sen from "../../styles/sementic.module.css";
+import Style from "./CustomTextField.module.css";
+
+interface TextFieldProps {
+  label: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+  placeholder?: string;
+}
+
+const CustomTextField: React.FC<TextFieldProps> = ({
+  label,
+  value,
+  onChange,
+  type = "text",
+  placeholder: place = "",
+}) => {
+  return (
+    <div className={Style.full}>
+      <div className={Style.box}>
+        <label className={`${Sen.headline_2} ${Style.labelColor}`}>
+          {label}
+        </label>
+        <input
+          className={`${Style.textField} ${Sen.label}`}
+          type={type}
+          placeholder={place}
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    </div>
+  );
+};
+export default CustomTextField;
