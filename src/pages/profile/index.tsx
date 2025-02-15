aed8684cf1b2f41a9911f3649967892493a32e7f
dlrjsgml44 <leegeh1213@gmail.com>
2025-02-16 03:49:17 +0900

    구독 유도

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,3 +1,14 @@
 export default function ProfilePage() {
-  return <div>profile</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <img style={{ width: "320px" }} src="/gosub.png" alt="머임" />
+    </div>
+  );
 }
